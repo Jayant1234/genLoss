21bc9f85c403f2097baffc87c705394815ffb09f
jp <31920414+Jayant1234@users.noreply.github.com>
2024-11-05 21:49:52 -0500

    bug fix

--- main_gen_loss_sign.py
+++ main_gen_loss_sign.py
@@ -422,7 +422,7 @@ def train_baseline(model, train_data, valid_data, optimizer, scheduler, device, 
                         # Compute gradient of s with respect to model parameters
                         grad_s = torch.autograd.grad(s, model.parameters())
                         if i%100: 
-                            print("gradient for coherence is:", grad_s))
+                            print("gradient for coherence is:", grad_s)
                             print("gradient for baseline is:", g_B1))
                             print("similarity of both gradients is::::",s)
                         # Compute total gradient
