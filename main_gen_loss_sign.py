6339c315d125322284c7b11619b831815e636dd8
jp <31920414+Jayant1234@users.noreply.github.com>
2024-11-06 20:08:57 -0500

    Revert "case of irregular batches found"
    This reverts commit 2ec9d545c3c670ac3c572639aa310df4436f3f38.

--- main_gen_loss_sign.py
+++ main_gen_loss_sign.py
@@ -399,60 +399,59 @@ def train_baseline(model, train_data, valid_data, optimizer, scheduler, device, 
                 if is_train:
                     with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_math=True, enable_mem_efficient=False):
                         model.zero_grad()
-                        if input.shape[-1] == b2_input.shape[-1]:
-                            g_B1 = torch.autograd.grad(L_B1, model.parameters(), create_graph=True)
-                            g_B2 = torch.autograd.grad(L_B2, model.parameters(), create_graph=True)
-                            
-                            #g_B1 = torch.autograd.grad(L_B1, model.parameters(), retain_graph=True)
-                            #g_B2 = torch.autograd.grad(L_B2, model.parameters(), retain_graph=True)
 
-                            # Convert gradients to vectors
-                            #g_B1_vector = torch.cat([g.view(-1) for g in g_B1])
-                            #g_B2_vector = torch.cat([g.view(-1) for g in g_B2])
+                        g_B1 = torch.autograd.grad(L_B1, model.parameters(), create_graph=True)
+                        g_B2 = torch.autograd.grad(L_B2, model.parameters(), create_graph=True)
+                        
+                        #g_B1 = torch.autograd.grad(L_B1, model.parameters(), retain_graph=True)
+                        #g_B2 = torch.autograd.grad(L_B2, model.parameters(), retain_graph=True)
 
-                            # Compute cosine similarity
-                            # cos_sim = torch.nn.functional.cosine_similarity(g_B1_vector, g_B2_vector, dim=0)
+                        # Convert gradients to vectors
+                        #g_B1_vector = torch.cat([g.view(-1) for g in g_B1])
+                        #g_B2_vector = torch.cat([g.view(-1) for g in g_B2])
 
-                            # # Define total loss
-                            # L_total = L_B1 - cos_sim
-                            # L_total.backward()
-                            # Compute dot product s = g_B2^T g_B1
-                            
-                            #s = sum((g1 * g2).sum() for g1, g2 in zip(g_B1, g_B2))
-                            cosine_sim = sum((g1 * g2).sum()/(torch.sqrt(sum((g1 ** 2)))*torch.sqrt(sum((g2 ** 2)))+ 1e-8) for g1, g2 in zip(g_B1, g_B2)) #trying pair-wise cosine similarity
-                            
-                            # Compute the norms of g_B1 and g_B2
-                            #norm_g_B1 = torch.sqrt(sum((g1 ** 2).sum() for g1 in g_B1))
-                            #norm_g_B2 = torch.sqrt(sum((g2 ** 2).sum() for g2 in g_B2))
+                        # Compute cosine similarity
+                        # cos_sim = torch.nn.functional.cosine_similarity(g_B1_vector, g_B2_vector, dim=0)
 
-                            # Normalize the dot product
-                            #cosine_sim = s / (norm_g_B1 * norm_g_B2 + 1e-8)  # Add epsilon for numerical stability
+                        # # Define total loss
+                        # L_total = L_B1 - cos_sim
+                        # L_total.backward()
+                        # Compute dot product s = g_B2^T g_B1
+                        
+                        #s = sum((g1 * g2).sum() for g1, g2 in zip(g_B1, g_B2))
 
-                            
-                            # Compute gradient of s with respect to model parameters
-                            grad_s = torch.autograd.grad((1-cosine_sim), model.parameters())
-                            if i % 10000 == 0: 
-                                #print("gradient for coherence is:", grad_s)
-                                #print("gradient for baseline is:", g_B1)
-                                print("similarity of both gradients is::::",cosine_sim)
-                            
-                            #curious case of barely doing it and still getting same results. 
-                            # if i >10: 
-                                # total_grad = [g1+g2 + 0*gs for g1,g2, gs in zip(g_B1, g_B2, grad_s)]
-                            # #Compute total gradient
-                            # else: 
-                                # total_grad = [g1+g2 + gs for g1,g2, gs in zip(g_B1, g_B2, grad_s)]
+                        cosine_sim = sum((g1 * g2).sum()/(torch.sqrt(sum((g1 ** 2)))*torch.sqrt(sum((g2 ** 2)))+ 1e-8) for g1, g2 in zip(g_B1, g_B2)) #trying pair-wise cosine similarity
 
-                            
-                            total_grad = [g1+g2 + gs for g1,g2, gs in zip(g_B1, g_B2, grad_s)]
-                            
-                            #Assign gradients to parameters
-                            for p, g in zip(model.parameters(), total_grad):
-                                p.grad = g
-                            #######
-                        else: 
-                            total_loss.backward()
-                            
+                        # Compute the norms of g_B1 and g_B2
+                        #norm_g_B1 = torch.sqrt(sum((g1 ** 2).sum() for g1 in g_B1))
+                        #norm_g_B2 = torch.sqrt(sum((g2 ** 2).sum() for g2 in g_B2))
+
+                        # Normalize the dot product
+                        #cosine_sim = s / (norm_g_B1 * norm_g_B2 + 1e-8)  # Add epsilon for numerical stability
+
+                        
+                        # Compute gradient of s with respect to model parameters
+                        grad_s = torch.autograd.grad((1-cosine_sim), model.parameters())
+                        if i % 10000 == 0: 
+                            #print("gradient for coherence is:", grad_s)
+                            #print("gradient for baseline is:", g_B1)
+                            print("similarity of both gradients is::::",cosine_sim)
+                        
+                        #curious case of barely doing it and still getting same results. 
+                        # if i >10: 
+                            # total_grad = [g1+g2 + 0*gs for g1,g2, gs in zip(g_B1, g_B2, grad_s)]
+                        # #Compute total gradient
+                        # else: 
+                            # total_grad = [g1+g2 + gs for g1,g2, gs in zip(g_B1, g_B2, grad_s)]
+
+                        
+                        total_grad = [g1+g2 + gs for g1,g2, gs in zip(g_B1, g_B2, grad_s)]
+                        
+                        #Assign gradients to parameters
+                        for p, g in zip(model.parameters(), total_grad):
+                            p.grad = g
+                        #######
+
                         trigger = i < 500 if args.two_stage else False
 
                         if args.filter == "none":
