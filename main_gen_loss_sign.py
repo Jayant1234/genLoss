a62ccf3a3fe46c3ba5e0ccf37bb88d35ecf8996e
jp <31920414+Jayant1234@users.noreply.github.com>
2024-12-18 18:31:54 -0500

    new similarity type bug fix

--- main_gen_loss_sign.py
+++ main_gen_loss_sign.py
@@ -645,7 +645,7 @@ if __name__ == "__main__":
 
     #Generalization Loss
     parser.add_argument("--method_type", default="progressive_grad")
-    parser.add_argument("--similarity_type", type=str, choices=["euc", "cosine"] default="cosine")
+    parser.add_argument("--similarity_type", type=str, choices=["euc", "cosine"], default="cosine")
     parser.add_argument("--cosine_steps",type=int, default=10)
     parser.add_argument("--lambda_weight", type=float, default=0.9)
     parser.add_argument("--max_epochs",type=int, default=500)
