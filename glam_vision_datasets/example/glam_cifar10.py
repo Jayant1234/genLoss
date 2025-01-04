2e47e0cef0a248cd1350c64519ab1dd82fa0b82e
bhumika Gupta <bhumikagupta@bhumikas-MacBook-Air.local>
2025-01-03 22:09:46 -0500

    powerloss 0.5

--- glam_vision_datasets/example/glam_cifar10.py
+++ glam_vision_datasets/example/glam_cifar10.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
                 similarity = s / (norm_g_B1 * norm_g_B2 + 1e-8)  # Add epsilon for numerical stability
                 power_loss = torch.pow(1-similarity, 2)
                 # Compute gradient of s with respect to model parameters
-                grad_s = torch.autograd.grad((power_loss), model.parameters())
+                grad_s = torch.autograd.grad(0.5*(power_loss), model.parameters())
                 if steps % 1000 == 0: 
                         #print("gradient for coherence is:", grad_s)
                         #print("gradient for baseline is:", g_B1)
