9bd6dffc9fc685a657024369fa30612638251714
jp <31920414+Jayant1234@users.noreply.github.com>
2025-01-16 16:29:41 -0500

    lookahead with momentum

--- example/train_lookahead.py
+++ example/train_lookahead.py
@@ -15,26 +15,34 @@ from sam import SAM
 import torch
 
 class Lookahead(torch.optim.Optimizer):
-    def __init__(self, base_optimizer, alpha=0.5, k=5):
+    def __init__(self, base_optimizer, alpha=0.5, k=5, lk_momentum=0.5):
         if not 0.0 <= alpha <= 1.0:
             raise ValueError(f"Invalid alpha: {alpha}")
         if not k >= 1:
             raise ValueError(f"Invalid k: {k}")
 
+        if not 0.0 <= lk_momentum < 1.0:
+            raise ValueError(f"Invalid momentum: {lk_momentum}")
+
         self.base_optimizer = base_optimizer
         self.alpha = alpha
         self.k = k
-
+        self.lk_momentum = lk_momentum
         # Track number of "fast" updates so we know when to do the slow update
         self._step_count = 0
 
         # Copy of the fast params to “slow” buffer
         self.slow_params = []
+        self.momentum_buffer = []
+
         for group in self.base_optimizer.param_groups:
             sp = []
+            mb = []
             for p in group['params']:
                 sp.append(p.clone().detach())
+                mb.append(torch.zeros_like(p))
             self.slow_params.append(sp)
+            self.momentum_buffer.append(mb)
 
     @property
     def param_groups(self):
@@ -50,18 +58,31 @@ class Lookahead(torch.optim.Optimizer):
         """
         loss = self.base_optimizer.step(closure)
         self._step_count += 1
-
+        
         if self._step_count % self.k == 0:
+            
             # Slow update
-            for group_idx, group in enumerate(self.base_optimizer.param_groups):
-                for p_idx, p in enumerate(group['params']):
-                    if p.grad is None:
-                        continue
-                    slow = self.slow_params[group_idx][p_idx]
-                    # slow <- slow + alpha * (fast - slow)
-                    slow += self.alpha * (p.data - slow)
-                    # Then copy back to fast parameters
-                    p.data.copy_(slow)
+            with torch.no_grad():
+                for group_idx, group in enumerate(self.base_optimizer.param_groups):
+                    for p_idx, p in enumerate(group['params']):
+                        #if p.grad is None:
+                            #continue # this is a bug since it causes model to not update some param that may have values changed from previous gradients.
+                        
+                        slow = self.slow_params[group_idx][p_idx]
+                        momentum = self.momentum_buffer[group_idx][p_idx]
+                        
+                        # Compute the difference between fast and slow weights
+                        delta = p.data - slow
+
+                        # Update the momentum buffer in place
+                        momentum.mul_(self.lk_momentum).add_(delta)
+
+                        # Update the slow weights with the momentum buffer
+                        slow.add_(self.alpha * momentum)
+                        # # slow <- slow + alpha * (fast - slow)
+                        # slow += self.alpha * (p.data - slow)
+                        # Then copy back to fast parameters
+                        p.data.copy_(slow)
 
         return loss
 
@@ -77,13 +98,13 @@ if __name__ == "__main__":
     parser.add_argument("--label_smoothing", default=0.1, type=float, help="Use 0.0 for no label smoothing.")
     parser.add_argument("--learning_rate", default=0.1, type=float, help="Base learning rate at the start of the training.")
     parser.add_argument("--momentum", default=0.9, type=float, help="SGD Momentum.")
+    parser.add_argument("--lk_momentum", default=0.0, type=float, help="Momentum for lookahead")
+
     parser.add_argument("--threads", default=2, type=int, help="Number of CPU threads for dataloaders.")
     parser.add_argument("--rho", default=2.0, type=int, help="Rho parameter for SAM.")
     parser.add_argument("--weight_decay", default=0.0005, type=float, help="L2 weight decay.")
     parser.add_argument("--width_factor", default=8, type=int, help="How many times wider compared to normal ResNet.")
     parser.add_argument("--label", default="Baseline SGD", type=str, help="Label for the experiment.")
-    parser.add_argument("--method_type", default="lookahead", type=str, help="Label for the experiment.")
-    parser.add_argument("--k_same", default=5, type=int, help="k for the same batch repetition")
     parser.add_argument("--k", default=5, type=int, help="k for the lookahead")
     parser.add_argument("--alpha", default=0.5, type=float, help="k for the lookahead")
 
@@ -104,7 +125,7 @@ if __name__ == "__main__":
         momentum=args.momentum,
         weight_decay=args.weight_decay,
     )
-    optimizer = Lookahead(base_optimizer, alpha=args.alpha, k=args.k)
+    optimizer = Lookahead(base_optimizer, alpha=args.alpha, k=args.k, lk_momentum=args.lk_momentum)
     scheduler = StepLR(base_optimizer, args.learning_rate, args.epochs)
     
     epoch=0
@@ -112,117 +133,32 @@ if __name__ == "__main__":
         model.train()
         log.train(len_dataset=len(dataset.train))
 
-        if args.method_type =='lookahead': 
-            for batch in dataset.train:
-                inputs, targets = (b.to(device) for b in batch)
+        for batch in dataset.train:
+            inputs, targets = (b.to(device) for b in batch)
 
-                # # first forward-backward step
-                # enable_running_stats(model)
+            # # first forward-backward step
+            # enable_running_stats(model)
 
-                # Here i Will use the simple sgd optimizer
-                predictions = model(inputs)
-                loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
-                # print("Predictions Shape: ", predictions.shape)
+            # Here i Will use the simple sgd optimizer
+            predictions = model(inputs)
+            loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
+            # print("Predictions Shape: ", predictions.shape)
+            # print("Targets Shape: ", targets.shape)
+            # print("Loss Shape Before: ", loss.shape)
+            
+            optimizer.zero_grad()
+            loss.mean().backward()
+            optimizer.step()
+
+            with torch.no_grad():
+                correct = torch.argmax(predictions.data, 1) == targets
+                # print("Loss Shape After: ", loss.shape)
+                # print("Correct Shape: ", correct.shape)
                 # print("Targets Shape: ", targets.shape)
-                # print("Loss Shape Before: ", loss.shape)
-                
-                optimizer.zero_grad()
-                loss.mean().backward()
-                optimizer.step()
+                log(model, loss.cpu(), correct.cpu(), scheduler.lr())
+                scheduler(epoch)
+        epoch+=1
 
-                with torch.no_grad():
-                    correct = torch.argmax(predictions.data, 1) == targets
-                    # print("Loss Shape After: ", loss.shape)
-                    # print("Correct Shape: ", correct.shape)
-                    # print("Targets Shape: ", targets.shape)
-                    log(model, loss.cpu(), correct.cpu(), scheduler.lr())
-                    scheduler(epoch)
-            epoch+=1
-
-        elif args.method_type =='lookdeep': 
-            if not args.k == args.k_same:
-                raise ValueError(f"k values needs to be synced")
-            k=args.k_same
-            for batch in dataset.train:
-                inputs, targets = (b.to(device) for b in batch)
-                for i in range(k): 
-                    
-                    # enable_running_stats(model)
-
-                    predictions = model(inputs)
-                    loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
-                    # print("Predictions Shape: ", predictions.shape)
-                    # print("Targets Shape: ", targets.shape)
-                    # print("Loss Shape Before: ", loss.shape)
-                    
-                    optimizer.zero_grad()
-                    loss.mean().backward()
-                    optimizer.step()
-
-                    with torch.no_grad():
-                        correct = torch.argmax(predictions.data, 1) == targets
-                        # print("Loss Shape After: ", loss.shape)
-                        # print("Correct Shape: ", correct.shape)
-                        # print("Targets Shape: ", targets.shape)
-                        log(model, loss.cpu(), correct.cpu(), scheduler.lr())
-                        scheduler(epoch)
-            epoch+=k
-            
-        elif args.method_type =='lad': 
-            k=args.k_same
-            
-
-            for batch in dataset.train:
-                inputs, targets = (b.to(device) for b in batch)
-                #define the slow parameters for lookdeep part. 
-                slow_params = []
-                for group in optimizer.param_groups:
-                    sp = []
-                    for p in group['params']:
-                        sp.append(p.clone().detach())
-                    slow_params.append(sp)
-                for i in range(k): 
-                    
-                    # enable_running_stats(model)
-
-                    predictions = model(inputs)
-                    loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
-                    # print("Predictions Shape: ", predictions.shape)
-                    # print("Targets Shape: ", targets.shape)
-                    # print("Loss Shape Before: ", loss.shape)
-                    
-                    optimizer.zero_grad()
-                    loss.mean().backward()
-                    
-                    lr = optimizer.param_groups[0]["lr"]  # or however you extract LR
-                    
-                    with torch.no_grad():
-                        correct = torch.argmax(predictions.data, 1) == targets
-                        # print("Loss Shape After: ", loss.shape)
-                        # print("Correct Shape: ", correct.shape)
-                        # print("Targets Shape: ", targets.shape)
-                        log(model, loss.cpu(), correct.cpu(), scheduler.lr())
-                        scheduler(epoch)
-                        # 2) Manually update parameters ignoring momentum/WD:
-                        for group in optimizer.param_groups:
-                            for p in group["params"]:
-                                if p.grad is None:
-                                    continue
-                                # "Manual SGD" step: param = param - lr * grad
-                                p.data -= lr * p.grad
-                # Suppose self.slow_params[group_idx][p_idx] holds the "slow" copy
-                # of the parameter p in the real model.
-
-                with torch.no_grad():
-                    for group_idx, group in enumerate(optimizer.param_groups):
-                        for p_idx, p in enumerate(group["params"]):
-                            slow_p = slow_params[group_idx][p_idx]  # the "slow" parameter tensor
-                            #gradient update
-                            p.grad = args.alpha * (slow_p - p.data)
-                            p.data=slow_p
-
-                optimizer.step()
-            epoch+=k
         model.eval()
         log.eval(len_dataset=len(dataset.test))
 
