64ea3e2ddbd7560f4f055f9f162dfa0261da6fdd
jp <31920414+Jayant1234@users.noreply.github.com>
2025-01-20 01:56:55 -0500

    debug

--- example/train_baseline.py
+++ example/train_baseline.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
     )
     #optimizer = Lookahead(base_optimizer, alpha=args.alpha, k=args.k, lk_momentum=args.lk_momentum)
     #optimizer = multi_lookahead(base_optimizer, alpha=args.alpha_list, k=args.k_list,layers=args.layers, lk_momentum=args.lk_momentums)
-    scheduler = StepLR(base_optimizer, args.learning_rate, args.epochs)
+    scheduler = StepLR(optimizer, args.learning_rate, args.epochs)
     
     epoch=0
     while (epoch<=args.epochs):
